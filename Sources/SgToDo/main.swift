0f5c3b5e9c53d26c14c207c5414d5bde6b4c3ea3
eugenf <eugenf78@gmail.com>
2018-06-23 07:08:47 +0300

    added xcodeproj; added base server implementation

--- Sources/SgToDo/main.swift
+++ Sources/SgToDo/main.swift
@@ -1,1 +1,27 @@
-print("Hello, world!")
+import Seagull
+import NIOHTTP1
+
+func pingHandler(_ request: SgRequest, _ ctx: SgRequestContext) -> SgResult {
+    let status = ["active": true]
+    return ctx.encode(dict: status)
+}
+
+let logger = DefaultLogger()
+
+do {
+    logger.info("Starting SgToDo server")
+    
+    var router = Router()
+    
+    try router.add(method: .GET, relativePath: "/ping", handler: pingHandler)
+    
+    let engine = Engine(router: router)
+    try engine.run(host: "localhost", port: 8010)
+    
+    defer { try! engine.close() }
+    try engine.waitForCompletion()
+    
+} catch let err {
+    logger.error("Couldn't start server: \(err)")
+}
+
